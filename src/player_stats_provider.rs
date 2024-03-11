0beb4a5cfbcf69ccfc121ad6f669e79eee0cfca0
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-03-11 07:36:38 +0300

    Add PlayerStatsProvider::prepare_player_list.

--- src/player_stats_provider.rs
+++ src/player_stats_provider.rs
@@ -1,3 +1,21 @@
 pub(crate) trait PlayerStatsProvider {
     fn get_player_stats(&self, region: &String, player_names_with_tags: &Vec<String>) -> String;
+
+    fn prepare_player_list(
+        &self,
+        player_names_with_tags: &Vec<String>,
+        use_hyphen: bool,
+    ) -> String {
+        let mut players = String::new();
+        players.push_str(
+            player_names_with_tags
+                .iter()
+                .map(|player| player.replace(" ", "%20"))
+                .map(|player| player.replace("#", if use_hyphen { "-" } else { "%23" }))
+                .collect::<Vec<String>>()
+                .join(",")
+                .as_str(),
+        );
+        players
+    }
 }
