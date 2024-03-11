181cba87d700ed55408b31ad34d62039c10786d2
Eugene "Riketta" Chugaev <riketta@outlook.com>
2024-03-11 19:43:19 +0300

    Refactor.

--- src/player_stats_providers.rs
+++ src/player_stats_providers.rs
@@ -1,6 +1,5 @@
-
+pub mod stat_provider_deeplolgg;
 pub mod stat_provider_opgg;
 pub mod stat_provider_porofessorgg;
 pub mod stat_provider_porogg;
 pub mod stat_provider_ugg;
-pub mod stat_provider_deeplolgg;
